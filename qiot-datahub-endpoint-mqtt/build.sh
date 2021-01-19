a685634941abdb4af505b558a1ef2ab6cadb2dac
abattagl <abattagl@localhost>
2021-01-19 15:00:22 +0100

    Switch container image repo Update Quarkus version Rename root package

--- qiot-datahub-endpoint-mqtt/build.sh
+++ qiot-datahub-endpoint-mqtt/build.sh
@@ -1,5 +1,5 @@
 ./mvnw clean package -Pnative -Dquarkus.profile=staging -Dquarkus.native.container-build=true -Dquarkus.native.container-runtime=docker
-docker rmi quay.io/qiot/qiot-datahub-endpoint-mqtt
-docker build -f src/main/docker/Dockerfile.native -t quay.io/qiot/qiot-datahub-endpoint-mqtt .
-docker push quay.io/qiot/qiot-datahub-endpoint-mqtt
+docker rmi quay.io/qiotcovid19/qiot-datahub-endpoint-mqtt:1.0.1
+docker build -f src/main/docker/Dockerfile.native -t quay.io/qiotcovid19/qiot-datahub-endpoint-mqtt:1.0.1 .
+docker push quay.io/qiotcovid19/qiot-datahub-endpoint-mqtt:1.0.1
 #docker run -it --rm -p 5000:5000 --net host quay.io/qiot/qiot-datahub-endpoint-mqtt
