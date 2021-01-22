17c01970b41f06e61fbf3a4e42bcf83e4370ef85
abattagl <abattagl@localhost>
2021-01-22 09:28:30 +0100

    QoS lvl 2 for outbound messages bugfixes

--- qiot-datahub-endpoint-mqtt/build.sh
+++ qiot-datahub-endpoint-mqtt/build.sh
@@ -1,3 +1,4 @@
+chmod +x mvn*
 ./mvnw clean package -Pnative -Dquarkus.profile=staging -Dquarkus.native.container-build=true -Dquarkus.native.container-runtime=docker
 docker rmi quay.io/qiotcovid19/qiot-datahub-endpoint-mqtt:1.0.1
 docker build -f src/main/docker/Dockerfile.native -t quay.io/qiotcovid19/qiot-datahub-endpoint-mqtt:1.0.1 .
